41751b552b6b9429544d83202fb029b621038107
Hikmet Demir <hikmet548@gmail.com>
2024-10-21 15:58:38 +0100

    quick redirect fix (#435)

---
+++ conversational-ai-sdks/widget.js
@@ -0,0 +1,13 @@
+/* (function() {
+    // Create and append the script element
+    var script = document.createElement('script');
+    script.src = "https://elevenlabs.io/convai-widget/index.js";
+    script.async = true;
+    script.type = "text/javascript";
+    document.head.appendChild(script);
+
+    // Create and append the widget element
+    var widget = document.createElement('elevenlabs-convai');
+    widget.setAttribute('agent-id', 'oapIEuaQlE30RdjtC9BX');
+    document.body.appendChild(widget);
+})(); */
