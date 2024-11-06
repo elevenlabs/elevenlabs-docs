772ccc0230c11ba0475aeafc3bd736ad9f18ebbd
Jacob <64662184+aarthificial@users.noreply.github.com>
2024-11-06 01:23:39 +0100

    Update convai widget style (#468)

--- conversational-ai-sdks/widget.js
+++ conversational-ai-sdks/widget.js
@@ -9,37 +9,39 @@
     // Create and append the widget element
     var widget = document.createElement('elevenlabs-convai');
     widget.setAttribute('agent-id', 'oapIEuaQlE30RdjtC9BX');
+    widget.setAttribute('avatar-orb-color-1', '#4D9CFF');
+    widget.setAttribute('avatar-orb-color-2', '#9CE6E6');
     widget.innerHTML = `\
-    <form slot="terms">
-      <h1>Terms and conditions</h1>
+    <form slot="terms" class="prose text-sm">
+      <h3>Terms and conditions</h3>
       <p>
         By clicking "Continue," I agree to the Terms of Service, acknowledge
         ElevenLabs' Privacy Policy.
       </p>
       <p>
-        <label>
-          <input required="required" type="checkbox" />
+        <label class="flex gap-2">
+          <input class="h-5" required="required" type="checkbox" />
           I consent to the recording, collection and use of my voice and data
           derived from my voice to interpret my speech and provide customer
           support services.
         </label>
       </p>
       <p>
-        <label>
-          <input required="required" type="checkbox" />
+        <label class="flex gap-2">
+          <input class="h-5" required="required" type="checkbox" />
           I consent to sharing my voice and data derived from my voice with 
           third-party service providers to train and improve our customer 
           support models.
         </label>
       </p>
       <p>
-        <label>
-          <input required="required" type="checkbox" />
+        <label class="flex gap-2">
+          <input class="h-5" required="required" type="checkbox" />
           I understand that if I do not consent to the collection as 
           described above, ElevenLabs services cannot be provided to 
           me.
         </label>
       </p>
     </form>`;
-document.body.appendChild(widget);
+    document.body.appendChild(widget);
 })();
