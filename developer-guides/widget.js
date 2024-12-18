d5e9ce05b3d0040d204ee9943d3a3df3b5e7c48d
Louis Jordan <louisjoecodes@gmail.com>
2024-12-18 14:43:43 +0000

    feat: add tools

--- developer-guides/widget.js
+++ developer-guides/widget.js
@@ -1,20 +1,20 @@
-(function() {
+(function () {
   // Create and append the script element
-  var script = document.createElement('script');
+  var script = document.createElement("script");
   script.src = "https://elevenlabs.io/convai-widget/index.js";
   script.async = true;
   script.type = "text/javascript";
   document.head.appendChild(script);
 
   // hide convai on mobile
-  var wrapper = document.createElement('div');
+  var wrapper = document.createElement("div");
   wrapper.className = "hidden lg:block";
 
   // Create and append the widget element
-  var widget = document.createElement('elevenlabs-convai');
-  widget.setAttribute('agent-id', 'OZmXb9pmFsSkDE5dukJv');
-  widget.setAttribute('avatar-orb-color-1', '#4D9CFF');
-  widget.setAttribute('avatar-orb-color-2', '#9CE6E6');
+  var widget = document.createElement("elevenlabs-convai");
+  widget.setAttribute("agent-id", "OZmXb9pmFsSkDE5dukJv");
+  widget.setAttribute("avatar-orb-color-1", "#4D9CFF");
+  widget.setAttribute("avatar-orb-color-2", "#9CE6E6");
   widget.innerHTML = `\
   <form slot="terms" class="prose text-sm">
     <h3>Terms and conditions</h3>
@@ -47,7 +47,29 @@
       </label>
     </p>
   </form>`;
-  
+
+  // Add tools to the widget
+  widget.addEventListener("elevenlabs-convai:call", (event) => {
+    event.detail.config.clientTools = {
+      redirect: ({ path }) => {
+        console.log("redirectToDocs", path);
+        const router = window?.next?.router;
+        router.push(path);
+      },
+      redirectToEmail: ({ subject, body }) => {
+        const encodedSubject = encodeURIComponent(subject);
+        const encodedBody = encodeURIComponent(body);
+        window.open(
+          `mailto:team@elevenlabs.io?subject=${encodedSubject}&body=${encodedBody}`,
+          "_blank"
+        );
+      },
+      redirectToExternalURL: ({ url }) => {
+        window.open(url, "_blank", "noopener,noreferrer");
+      },
+    };
+  });
+
   // Append widget to wrapper, then wrapper to body
   wrapper.appendChild(widget);
   document.body.appendChild(wrapper);
