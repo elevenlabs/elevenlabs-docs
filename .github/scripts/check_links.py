ee592052765110c5ff2d0436435b05dee00f7c76
Angelo Giacco <giacco.angelo.s@gmail.com>
2025-03-26 14:22:13 +0000

    feat: bump docs to latest version of api spec and add changelog (#924)
    * feat: bump docs to latest version of api spec and add changelog
    
    * fix: add changelog generation prompt
    
    * fix links
    
    one has to be added manually
    
    * fix
    
    * fix
    
    * fix override
    
    * fix placement
    
    * fix: add v1 voices to legacy section
    
    * fix fmt
    
    * fix to list voices v2

--- .github/scripts/check_links.py
+++ .github/scripts/check_links.py
@@ -553,6 +553,8 @@ def validate_links(docs_dir, valid_paths, api_reference_paths):
     
     special_case_paths = {
         "/docs/conversational-ai/api-reference/conversational-ai/websocket",
+        "/docs/api-reference/phone-numbers/twilio-outbound-call",
+        "/docs/api-reference/phone-numbers/create-phone-number"
     } #todo angelo: fix code so we can remove this 
     valid_paths.update(special_case_paths)
     
